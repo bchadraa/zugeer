0f78d16c360ffa3969a5c8d4184aaf3ab3679aac
Bata Chadraa <bchadraa@gmail.com>
2012-08-15 23:04:39 -0700

    layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,42 +5,39 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
       page.should have_selector('h1', :text => 'App1')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
       page.should have_selector('title',
                         :text => "Learning Ruby as App1 | Home")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
       page.should have_selector('title',
                         :text => "Learning Ruby as App1 | Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "Learning Ruby as App1 | About Us")
     end
